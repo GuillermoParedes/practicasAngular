05fc343e84626104f860fe66740f9aa3a46426fc
GuiLLeRm0 <gdavid.ptorrez@gmail.com>
2015-03-18 15:16:49 -0400

    Commit 3.1

---
+++ assets/js/filtros.js
@@ -0,0 +1,36 @@
+/* 
+    @uthor: Guillermo David Paredes Torrez
+    Contact: gdavid.ptorrez@gmail.com
+    skype : Guillermo Paredes gdavid_ptorrez@hotmail.com
+*/
+var app = angular.module("myApp",[]);
+
+  app.controller('ControladorFiltros', function($scope) {
+  $scope.empleados = [
+      {
+        nombre:'Ana', paterno: 'Guzman', materno: 'Guzman', primerDia: new Date(),
+        salario: 12000, telefono:'5587687687', bono: 1.456789
+      },
+      {
+        nombre:'Adrian', paterno: 'Romero', materno: 'Paez', primerDia:  new Date(),
+        salario: 12000, telefono:'5512345678', bono: 9.654321
+      },
+      {
+        nombre:'Rodolfo', paterno: 'Solares', materno: 'Madero', primerDia:  new Date(),
+        salario: 14000, telefono:'5587654321', bono: 7.333333
+      },
+      {
+        nombre:'Manuel', paterno: 'Perez', materno: 'Solin', primerDia:  new Date(),
+        salario: 11000, telefono:'5518273645', bono: 5.272727
+      },
+      {
+        nombre:'Dana', paterno: 'Roman', materno: 'Herrera', primerDia:  new Date(),
+        salario: 20000, telefono:'5581726354', bono: 1.444444
+      },
+      {
+        nombre:'Alejandro', paterno: 'Mena', materno: 'Morales', primerDia:  new Date(),
+        salario: 5000, telefono:'5512312312', bono: 12.989898
+      }
+    ];
+
+});
