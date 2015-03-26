35b861fb39732d078064c1bab82f7e1d2d24d141
GuiLLeRm0 <gdavid.ptorrez@gmail.com>
2015-03-26 12:07:08 -0400

    Commit 4.3

--- assets/js/fabricas.js
+++ assets/js/fabricas.js
@@ -5,15 +5,38 @@
 */
 
 var app = angular.module("myApp", []);
-app.factory('Dato',function(){
-    return {mensaje : 'Saludos desde la Fabrica'}
+app.factory('Fabrica',function(){
+    var servicio = {
+        objecto : { mensaje : 'Saludos desde la fabrica'},
+        msjInicial : function(){
+            servicio.objecto['mensaje'] = 'Saludos desde la fabrica';
+        },
+        msjNuevo : function(msj){
+            servicio.objecto.mensaje = msj;
+        }
+    };
+    
+    return servicio;
 });
-app.controller('ControladorUno', function($scope, Dato){
-    $scope.dato = Dato;
+app.controller('ControladorUno', function($scope, Fabrica){
+    $scope.nuevo = function(){
+        Fabrica.msjNuevo($scope.nuevoMensaje);
+    }
+    
+    $scope.dato = Fabrica.objecto;
 });
 
-app.controller('ControladorDos', function($scope,Dato){
-   $scope.dato = Dato;
+app.controller('ControladorDos', function($scope,Fabrica){
+   $scope.nuevo = function(){
+        Fabrica.msjNuevo($scope.nuevoMensaje);
+    }
+    $scope.dato = Fabrica.objecto;
 });
 
-
+app.controller('ControladorTres', function($scope, Fabrica){
+    $scope.resetear = function(){
+        Fabrica.msjInicial();
+    }
+    
+});
+//av montenegro edif loyanda e8 piso 1 oficina 1 18 calacoto 3 
